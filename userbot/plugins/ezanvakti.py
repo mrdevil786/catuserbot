b0fe1f8046db0c4251b4b89473c6b24ba946b3d4
mrdevil786 <40206606+mrdevil786@users.noreply.github.com>
2021-04-02 23:54:05 +0530

    Update ezanvakti.py

--- userbot/plugins/ezanvakti.py
+++ userbot/plugins/ezanvakti.py
@@ -38,7 +38,7 @@ async def get_adzan(adzan):
 CMD_HELP.update(
     {
         "ezanvakti": "**Plugin : **`ezanvakti`\
-    \n\n**Syntax : **`.ezanvakti <city name>`\
+    \n\n**Syntax : **`.azanwaqt <city name>`\
     \n**Function : **__Shows you the Islamic prayer times of the given city name__"
     }
 )
